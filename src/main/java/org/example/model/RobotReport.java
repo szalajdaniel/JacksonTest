1c762e08a003d3bb267b666560c53ae64ddd8815
szalajdaniel <100935588+szalajdaniel@users.noreply.github.com>
2026-09-06 22:10:13 +0200

    Delete src directory

--- src/main/java/org/example/model/RobotReport.java
+++
@@ -1,9 +0,0 @@
-package org.example.model;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import java.time.LocalDate;
-
-public record RobotReport(String serialNumber, @JsonFormat(pattern = "dd-MM-yyyy") LocalDate commissionDate,
-                          int batettery) {
-}
